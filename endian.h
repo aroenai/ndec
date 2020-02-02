e049ca6329ad4f099baffafda2e9200db523fe7f
Aroenai <28711790+aroenai@users.noreply.github.com>
2020-02-02 13:16:18 -0700

    Resolve compiler error
    Change inline to static inline.

--- endian.h
+++ endian.h
@@ -3,7 +3,7 @@
 typedef uint16_t u16;
 typedef uint32_t u32;
 
-inline u32 swap32(u32 swapme)
+static inline u32 swap32(u32 swapme)
 {
 	return (((swapme & 0xFF000000) >> 24)
 		   |((swapme & 0x00FF0000) >>  8)
